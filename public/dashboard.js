42fe23001dfb31d88960c8fbe0dab193decc7398
unknown <thediamondknight22@gmail.com>
2026-07-15 17:07:06 -0400

    Created Weekly mood list with days on dashboard.

---
+++ public/dashboard.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    fetch('/api/users')
+        .then(response => response.json())
+        .then(data => {
+            var user = data.users[0];
+            var moodHistory = user.moodHistory;
+            document.getElementById('greeting').textContent = 'Hello ' + user.name + '! I hope your day is going well.';
+            buildWeeklyOverview(moodHistory);
+        });
+    });
+    
+    function buildWeeklyOverview(moodHistory){
+        var today = new Date();
+        for (var i = 6; i >= 0; i--){
+            var day = new Date(today);
+            day.setDate(today.getDate() - i);
+            var dateString = day.toISOString().split('T')[0];
+            var dayName = day.toLocaleDateString('en-US', { weekday : 'short'});
+            console.log(dateString);
+            console.log(dayName);
+            var entry = moodHistory.find(function(e){
+                return e.date === dateString;
+            });
+             var moodText = entry ? entry.mood : 'No entry';
+             var card = document.createElement('div');
+             card.classList.add('day-card');
+             card.innerHTML = '<p>' + dayName + '</p><p>' + moodText + '</p>';
+             document.getElementById('week-grid').appendChild(card);
+        }
+
+    }
